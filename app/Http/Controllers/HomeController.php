9e12874cb883c3e1fc1e6f83481c985c302abd7a
Swytos <svatoslavzivotovskij@gmail.com>
2019-08-09 13:29:00 +0300

    - test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,6 +20,7 @@ class HomeController extends Controller
     }
 
     public function mailContact(CreateContact $request)
+        
     {
         $data['name'] = $request->name;
         $data['email'] = $request->email;
