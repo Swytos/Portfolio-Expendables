4d137f1e9aa7043ae0c619e5e3cb46f762200c9a
Swytos <svatoslavzivotovskij@gmail.com>
2019-08-15 11:26:03 +0300

    -add padding

--- resources/views/portfolio/sections/services.blade.php
+++ resources/views/portfolio/sections/services.blade.php
@@ -2,7 +2,6 @@
 <section id="services">
     <div class="container">
         <div class="row">
-
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading text-uppercase">Services</h2>
                 <h3 class="section-subheading text-muted">A small range of services that we can provide.</h3>
